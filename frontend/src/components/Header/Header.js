776b516b5755f3e5734af680bd9b250031c214af
Vikkesh <fieryvikkesh@gmail.com>
2025-01-20 02:17:11 +0530

    Added cartpage, searchbar, tags, thumbnails, title,

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import classes from './header.module.css';
 import { Link } from 'react-router-dom';
+import { useCart } from '../../hooks/useCart';
 export default function Header() {
     const user = {
         name: "Rahul",
     };
-    const cart = {
-        totalCount: 10,
-    };
+    const {cart} = useCart();
     const logout = () => { }
 
 
