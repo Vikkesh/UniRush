e2de2cbc03317d8a5f0b3bf5454ba632b9b6f618
Vikkesh <fieryvikkesh@gmail.com>
2025-02-03 02:07:05 +0530

    Made loading page

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -9,15 +9,17 @@ import './axiosConfig';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { AuthProvider } from './hooks/useAuth';
+import { LoadingProvider } from './hooks/useLoading';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <AuthProvider>
-        <CartProvider>
-          <App />
-          <ToastContainer
+      <LoadingProvider>
+        <AuthProvider>
+          <CartProvider>
+            <App />
+            <ToastContainer
               position="bottom-right"
               autoClose={5000}
               hideProgressBar={false}
@@ -29,8 +31,9 @@ root.render(
               pauseOnHover
               theme="light"
             />
-        </CartProvider>
-      </AuthProvider>
+          </CartProvider>
+        </AuthProvider>
+      </LoadingProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
