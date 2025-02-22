d48435277d4b30e8cd2e84b4bd5af80073ac68ce
Vikkesh <fieryvikkesh@gmail.com>
2025-02-23 00:54:08 +0530

    Deploying webapp to render

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,6 @@
 import dotenv from 'dotenv';
 dotenv.config();
+import { fileURLToPath } from 'url';
 import express from 'express';
 import cors from 'cors';
 import foodRouter from './routers/food.router.js';
@@ -7,12 +8,14 @@ import userRouter from './routers/user.router.js';
 import orderRouter from './routers/order.router.js';
 import razorpayRouter from './routers/razorpay.router.js';
 import { dbconnect } from './config/database.config.js';
-
+import path, { dirname } from 'path';
 dbconnect();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const app = express();
-
 app.use(express.json());
-
 app.use(
   cors({
     credentials: true,
@@ -25,8 +28,15 @@ app.use('/api/users', userRouter);
 app.use('/api/orders', orderRouter);
 app.use('/api/razorpay', razorpayRouter);
 
-const PORT = 5000;
+const publicFolder = path.join(__dirname, 'public');
+app.use(express.static(publicFolder));
+
+app.get('*', (req, res) => {
+  const indexFilePath = path.join(publicFolder, 'index.html');
+  res.sendFile(indexFilePath);
+});
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log('listening on port ' + PORT);
-  }
-);
+  console.log('listening on port ' + PORT);
+});
